735b12a5ff72a3785b0c63494c289891444d54eb
Jamie Michelle Greene <Jamie.M.Greene@aexp.com>
2017-01-10 14:24:00 -0700

    fxml

---
+++ src/SimpleUI/SimpleUI.java
@@ -0,0 +1,27 @@
+package SimpleUI;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class SimpleUI extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		HBox box = new HBox();
+		VBox vbox = new VBox();
+		
+}
+
+
+
+
+
+
+public static void main(String[]args) {
+		Application.launch(args);
+	}
+}
